2ecd3f71c9eb8bd5537aaf4d9000900679f14957
Michael <michaeljrdeitz@gmail.com>
2024-09-24 20:52:46 -0500

    Started making the enemy class with animation and making a start and reset with the enemy

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-class Enemy {
-
-}
 
 class Game {
-    constructor(canvas){
+    constructor(canvas, ctx){
         this.canvas = canvas;
+        this.ctx = ctx;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.enemy1 = new Enemy(this);
+
+        this.start();
 
         window.addEventListener('resize', e => {
             this.resize(e.target.innerWidth, e.target.innerHeight)
         });
+    }
+    start(){
+        this.resize(window.innerWidth, window.innerHeight);
     }
     resize(width, height){
         this.canvas.width = width;
         this.canvas.height = height;
         this.width = width;
         this.height = height;
+       
+    }
+    render(){
+        this.enemy1.update();
+        this.enemy1.draw();
     }
 }
 
@@ -24,7 +33,15 @@ window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.height = window.innerHeight; 
+    
 
-    const game = new Game(canvas);
+    const game = new Game(canvas, ctx);
+    
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        game.render(ctx)
+        requestAnimationFrame(animate)
+    }
+    requestAnimationFrame(animate)
 })
